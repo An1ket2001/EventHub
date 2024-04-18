620bb970bdaeb1a47fba4eb72910fcb0571a46bf
Harsh Agarwal <harsh.e.agarwal@pwc.com>
2024-04-18 18:32:58 +0530

    added error bar in the login item when wrong credentials are given

--- frontend/src/components/Auth/LoginSignup.js
+++ frontend/src/components/Auth/LoginSignup.js
@@ -16,7 +16,7 @@ const LoginSignup = () => {
   const [designation, setDesignation] = useState("");
   const [errors, setErrors] = useState({});
   const [designationList, setDesignationList] = useState([]);
-  const [runspinner,setrunspinner]=useState(false);
+  const [runspinner, setrunspinner] = useState(false);
 
   const handleActionChange = (newAction) => {
     setAction(newAction);
@@ -46,73 +46,74 @@ const LoginSignup = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const validationErrors = {};
-
     if (action === "Login") {
-      
+
       if (!email) {
         validationErrors.email = "Email is required";
-        
+
       }
       if (!password) {
         validationErrors.password = "Password is required";
-        
+
       } else if (password.length < 6) {
         validationErrors.password = "Password should be at least 6 characters long";
-        
+
       }
-     
-  
+
+
     } else {
-      
+
       if (!username) {
         validationErrors.username = "Username is required";
-        
-        
+
+
       }
       if (!email) {
         validationErrors.email = "Email is required";
-        
+
       } else if (!/\S+@\S+\.\S+/.test(email)) {
         validationErrors.email = "Invalid email format";
-       
+
       }
       if (!password) {
         validationErrors.password = "Password is required";
-        
+
       } else if (password.length < 6) {
         validationErrors.password = "Password should be at least 6 characters long";
-        
+
       }
       if (!confirmPassword) {
         validationErrors.confirmPassword = "Confirm Password is required";
-       
+
       } else if (confirmPassword !== password) {
         validationErrors.confirmPassword = "Passwords do not match";
-        
+
       }
       if (!designation) {
         validationErrors.designation = "Designation is required";
-        
+
       }
-      
+
     }
 
     if (Object.keys(validationErrors).length === 0) {
       if (action === "Login") {
         AuthApi("login", { email, password });
-        toast.success("Successfully LoggedIn!");
+
       } else {
         AuthApi("createuser", { "name": username, email, password, "designationId": designation });
-        toast.success("Successfully SignnedUp!");
+        // toast.success("Successfully SignnedUp!");
       }
       setErrors({});
     } else {
       setErrors(validationErrors);
     }
+
   };
 
   const AuthApi = async (endpoint, data) => {
     setrunspinner(true);
+
     const res = await fetch(`http://localhost:5000/api/auth/${endpoint}`, {
       method: "POST",
       headers: {
@@ -120,11 +121,20 @@ const LoginSignup = () => {
       },
       body: JSON.stringify(data)
     })
-    const userData = await res.json();
-    localStorage.setItem("user", JSON.stringify(userData));
-    auth.login();
-    setrunspinner(false);
-    navigate("/");
+    if (res.ok) {
+      const userData = await res.json();
+      localStorage.setItem("user", JSON.stringify(userData));
+      auth.login();
+      setrunspinner(false);
+      if (endpoint === "login")
+        toast.success("Successfully LoggedIn!");
+      else
+        toast.success("Successfully SignnedUp!");
+      navigate("/");
+    } else {
+      setrunspinner(false);
+      toast.error("Invalid Credentials");
+    }
   }
 
   useEffect(() => {
@@ -138,62 +148,62 @@ const LoginSignup = () => {
 
   return (
     <>
-    {runspinner && <Spinner />}
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.text}>{action}</div>
-        <div className={styles.underline}></div>
-      </div>
-      <div className={styles.inputs}>
-        {action === "Sign Up" && (<div className={styles.input}>
-          <input className={styles.inputBx} type={styles.text} placeholder='Username' value={username} onChange={handleUsernameChange} />
-          {errors.username && <span className={styles.error}>{errors.username}</span>}
+      {runspinner && <Spinner />}
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.text}>{action}</div>
+          <div className={styles.underline}></div>
         </div>
-        )}
-
-        <div className={styles.input}>
-          <input className={styles.inputBx} type="email" placeholder='Email' value={email} onChange={handleEmailChange} />
-          {errors.email && <span className={styles.error}>{errors.email}</span>}
-        </div>
-
-
-        <div className={styles.input}>
-          <input className={styles.inputBx} type="password" placeholder='Password' value={password} onChange={handlePasswordChange} />
-          {errors.password && <span className={styles.error}>{errors.password}</span>}
-        </div>
-
-        {action === "Sign Up" && (
-          <div className={styles.input}>
-            <input className={styles.inputBx} type="password" placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
-            {errors.confirmPassword && <span className={styles.error}>{errors.confirmPassword}</span>}
+        <div className={styles.inputs}>
+          {action === "Sign Up" && (<div className={styles.input}>
+            <input className={styles.inputBx} type={styles.text} placeholder='Username' value={username} onChange={handleUsernameChange} />
+            {errors.username && <span className={styles.error}>{errors.username}</span>}
           </div>
-        )}
+          )}
 
-        {action === "Sign Up" && (
           <div className={styles.input}>
-            <select value={designation} onChange={handleDesignationChange} className={styles.selectBx}>
-              <option>Select Designation</option>
-              {
-                designationList.map((desg) => {
-                  return <option key={desg._id} value={desg._id}>{desg.designation}</option>
-                })
-              }
-            </select>
-            {errors.designation && <span className={styles.error}>{errors.designation}</span>}
+            <input className={styles.inputBx} type="email" placeholder='Email' value={email} onChange={handleEmailChange} />
+            {errors.email && <span className={styles.error}>{errors.email}</span>}
           </div>
-        )}
-      </div>
 
-      {action === "Sign Up" ? <div></div> : <div className={styles.forgotpassword}>Forgot Password?<span>Click Here! </span></div>}
 
-      <div className={styles.submitContainer}>
-        <button className={styles.submit} onClick={handleSubmit}>{action}</button>
-        <button className={`${styles.submit} ${styles.gray}`} onClick={() => handleActionChange(action === "Login" ? "Sign Up" : "Login")}>
-          {action === "Login" ? "Sign Up" : "Login"}
-        </button>
-        <ToastContainer />
+          <div className={styles.input}>
+            <input className={styles.inputBx} type="password" placeholder='Password' value={password} onChange={handlePasswordChange} />
+            {errors.password && <span className={styles.error}>{errors.password}</span>}
+          </div>
+
+          {action === "Sign Up" && (
+            <div className={styles.input}>
+              <input className={styles.inputBx} type="password" placeholder='Confirm Password' value={confirmPassword} onChange={handleConfirmPasswordChange} />
+              {errors.confirmPassword && <span className={styles.error}>{errors.confirmPassword}</span>}
+            </div>
+          )}
+
+          {action === "Sign Up" && (
+            <div className={styles.input}>
+              <select value={designation} onChange={handleDesignationChange} className={styles.selectBx}>
+                <option>Select Designation</option>
+                {
+                  designationList.map((desg) => {
+                    return <option key={desg._id} value={desg._id}>{desg.designation}</option>
+                  })
+                }
+              </select>
+              {errors.designation && <span className={styles.error}>{errors.designation}</span>}
+            </div>
+          )}
+        </div>
+
+        {action === "Sign Up" ? <div></div> : <div className={styles.forgotpassword}>Forgot Password?<span>Click Here! </span></div>}
+
+        <div className={styles.submitContainer}>
+          <button className={styles.submit} onClick={handleSubmit}>{action}</button>
+          <button className={`${styles.submit} ${styles.gray}`} onClick={() => handleActionChange(action === "Login" ? "Sign Up" : "Login")}>
+            {action === "Login" ? "Sign Up" : "Login"}
+          </button>
+          <ToastContainer />
+        </div>
       </div>
-    </div>
     </>
   )
 }
